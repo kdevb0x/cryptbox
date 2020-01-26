1784f78dd74ff12a3b6d3aec65404317297a21dd
Kristofer Davidson <kdevb0x@gmail.com>
2020-01-26 14:28:55 -0700

    late update

--- cryptbox.go
+++ cryptbox.go
@@ -2,6 +2,7 @@
 // This software may be modified and distributed under the terms
 // of the MIT license.  See the LICENSE file for details.
 
+// Package cryptbox is a simple encrypted container for storage of arbitrary files.
 package cryptbox
 
 import (
@@ -11,53 +12,56 @@ import (
 	"crypto/rand"
 	"crypto/sha512"
 	"errors"
-	"hash/crc32"
 	"io"
 	"os"
-	"unsafe"
 
 	"golang.org/x/crypto/bcrypt"
 	"golang.org/x/crypto/pbkdf2"
+	"golang.org/x/crypto/sha3"
 )
 
 var (
-	ErrFileIOError = errors.New("encountered critical error during file IO operation")
+	ErrFileIOError = errors.New("encountered critical error during file io operation")
 )
+
+var SaveDir, _ = os.UserHomeDir()
 
 type Crypter interface {
 	Seal(password []byte, salt ...[]byte) error
 	Sealed() bool
-	Unseal(password []byte) error
-}
-
-type Jsonmeta struct {
-	Filename string `json:"filename"`
+	Open(password []byte) error
 }
 
 // Cryptbox is an abstract object that stores sensitive files securely (for backup).
 type Cryptbox struct {
-	Metadata map[uint32]FileRepr // crc32 checksum to file metadata
-	data     unsafe.Pointer
+	sums map[[64]byte]file // uses sha-3 SHAKE256 hash for keys
+
+	data uintptr
 }
 
-// FileRepr is a representation of a filesystem file, it contains the *os.File,
+// file is a representation of a filesystem file, it contains the *os.File,
 // but also an internal buffer holding the  actual bytes that make up the file.
-type FileRepr struct {
+type file struct {
 	Filename string
 	Fd       *os.File
 	Size     int64
-	// Buff     bytes.Buffer  // uneeded ATM
+	Buff     *bytes.Buffer // uneeded ATM
 }
 
 func NewCryptBox() *Cryptbox {
+	// TODO: Create filedes and mmap() with posix_typed_mem_open(filepath string, os.O_RDWR, POSIX_TYPED_MEM_ALLOCATABLE)
+	// the goal is to use mmaped data for storage of encrypted data.
+
+	// data, err := unix.Creat(SaveDir, os.ModeExclusive)
+
 	return &Cryptbox{
-		Metadata: make(map[uint32]FileRepr),
-		data:     nil,
+		sums: make(map[[64]byte]file),
+		data: 0,
 	}
 }
 
-// OpenFromFilepath opens a file from disk, and creates a FileRepr we can use.
-func OpenFromFilepath(path string) (*FileRepr, error) {
+// openFromFilepath opens a file from disk, and creates a file we can use.
+func openFromFilepath(path string) (*file, error) {
 	f, err := os.Open(path)
 	if err != nil {
 		return nil, err
@@ -67,7 +71,7 @@ func OpenFromFilepath(path string) (*FileRepr, error) {
 		return nil, err
 	}
 
-	file := &FileRepr{
+	file := &file{
 		Filename: f.Name(),
 		Fd:       f,
 		Size:     size.Size(),
@@ -76,10 +80,10 @@ func OpenFromFilepath(path string) (*FileRepr, error) {
 	return file, nil
 }
 
-func (b *Cryptbox) AddFile(f *FileRepr) error {
+func AddFile(to Crypter, f *file) error {
 
 	// TODO: Finish implementing this.
-	// Currently we don't read the bytes from FileRepr underlying os.File
+	// Currently we don't read the bytes from file underlying os.File
 	// untill we iterate throught the Metadat map, but we need a []byte in
 	// order to hash for the maps key, so kind of a chicken before egg issue.
 
@@ -106,7 +110,7 @@ func (b *Cryptbox) Seal(password []byte) error {
 	}
 	// pack each File contiguously
 
-	cipherReadWriter := gcm.Seal(nil, nonce, buff.Bytes(), nil)
+	cipherReadWriter := gcm.Seal(nil, nonce, buff.Bytes(), nonce)
 
 	// append nonce to encrypted data
 	cipherReadWriter = append(cipherReadWriter, nonce...)
@@ -116,13 +120,13 @@ func (b *Cryptbox) Seal(password []byte) error {
 // packFileByteSlice mimics packFileBuffs but returns a []byte, not bytes.Buffer.
 func (b *Cryptbox) packFileByteSlice() ([]byte, error) {
 	var totalLen int64
-	for _, t := range b.Metadata {
+	for _, t := range b.sums {
 		totalLen += t.Size
 	}
 	var tmpb = make([]byte, totalLen)
 	// mainbuff := bytes.NewBuffer(tmpb)
 	var last int
-	for _, tb := range b.Metadata {
+	for _, tb := range b.sums {
 		// n, err := io.Copy(mainbuff, tb.Fd)
 		// if err != nil {
 		// 	return ErrFileIOError
@@ -147,16 +151,16 @@ func (b *Cryptbox) packFileByteSlice() ([]byte, error) {
 
 }
 
-// packFileBuffs appends the bytes from each FileRepr to the pre-encrypted buffer,
+// packFileBuffs appends the bytes from each file to the unencrypted buffer,
 // making sure to clone each Fd in turn.
 func (b *Cryptbox) packFileBuffs() (*bytes.Buffer, error) {
 	var totalLen int64
-	for _, t := range b.Metadata {
+	for _, t := range b.sums {
 		totalLen += t.Size
 	}
 	var tmpb = make([]byte, totalLen)
 	mainbuff := bytes.NewBuffer(tmpb)
-	for _, tb := range b.Metadata {
+	for _, tb := range b.sums {
 		n, err := io.Copy(mainbuff, tb.Fd)
 		if err != nil {
 			return nil, ErrFileIOError
@@ -165,11 +169,12 @@ func (b *Cryptbox) packFileBuffs() (*bytes.Buffer, error) {
 		tb.Fd.Close()
 	}
 	if totalLen != 0 {
-		return ErrFileIOError
+		return nil, ErrFileIOError
 	}
-	return nil
+	return mainbuff, nil
 }
 
-func getcrc32(f *FileRepr) uint32 {
-	return crc32.ChecksumIEEE(f.Buff.Bytes())
+func getSHA3Hash(f *file) [64]byte {
+	h := sha3.NewShake256()
+
 }
--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module cryptbox
 
 go 1.12
 
-require golang.org/x/crypto v0.0.0-20190325154230-a5d413f7728c
+require (
+	golang.org/x/crypto v0.0.0-20190513172903-22d7a77e9e5f
+	golang.org/x/sys v0.0.0-20190520201301-c432e742b0af // indirect
+)
